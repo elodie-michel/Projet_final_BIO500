d2d21443117ef45562ee2a8c4cacc23a41ee6c1a
dan-2609 <danae.vaillancourt@usherbrooke.ca>
2025-03-29 16:15:21 -0400

    J'ai modifié script_principal pour ajouter le SQL

--- lepidopteres/Script_principal.R
+++ lepidopteres/Script_principal.R
@@ -4,34 +4,9 @@ source("Script_nettoyage.R")
 source("Script_test_erreur.R")
 source("Script_table.R")
 
-# Lire les fichiers CSV 
+# Traitement de données
 dossier <- "./donnees"
-liste_donnees <- lire_csv(dossier)
-
-# Appliquer les différentes transformations
-liste_donnees <- ajouter_temps_fictif(liste_donnees)
-non_numeric_values <- verifier_values_non_numeriques(liste_donnees)
-
-# Corriger les années spécifiques
-liste_donnees <- corriger_annee(liste_donnees, index = which(names(liste_donnees) == "d_1985"), 1985)
-
-liste_donnees <- corriger_annee(liste_donnees, index = which(names(liste_donnees) == "d_1987"), 1987)
-
-# Convertir les colonnes year_obs et day_obs
-liste_donnees <- convertir_annee_et_jour(liste_donnees)
-
-# Fusionner les bases de données
-g_b <- fusionner_donnees(liste_donnees)
-
-# Nettoyer et convertir les colonnes spécifiques
-g_b <- nettoyer_et_convertir_colonnes(g_b)
-
-#Suprimer des colonnes non-nécessaire
-# Liste des colonnes à supprimer
-colonnes_a_supprimer <- c("obs_unit")
-
-# Appliquer la fonction pour supprimer ces colonnes
-g_b <- supprimer_colonnes_non_necessaires(g_b, colonnes_a_supprimer)
+g_b <- traiter_donnees(dossier)
 
 # Affichage des résultats
 print(head(g_b))
@@ -71,3 +46,52 @@ table_temps <- creer_table_2 (g_b)
 
 #Création de la table des droits (id_droits, creator, title, publisher, intellectual_rights, license, owner, original_source)
 table_droits <- creer_table_3(g_b)
+
+
+#install.packages('RSQLite')
+library(RSQLite)
+con <- dbConnect(SQLite(), dbname="lepidopteres.db")
+
+creer_obs <-
+  'CREATE TABLE observations (
+id_obs INTEGER NOT NULL,
+observed_scientific_name VARCHAR(100),
+lat REAL,
+lon REAL,
+obs_value  INTERGER,
+id_droits INTEGER NOT NULL,
+PRIMARY KEY (id_obs)
+);'
+dbSendQuery(con, creer_obs)
+
+creer_temps <-
+  'CREATE TABLE temps (
+id_obs INTEGER NOT NULL,
+dwc_event_date DATE,
+year_obs INTEGER,
+day_obs INTEGER,
+time_obs TIME,
+PRIMARY KEY (id_obs),
+FOREIGN KEY (id_obs) REFERENCES observations(id_obs)
+);'
+dbSendQuery(con, creer_temps)
+
+creer_droits <-
+  'CREATE TABLE droits (
+id_droits INTEGER NOT NULL,
+creator VARCHAR(100),
+title VARCHAR(100),
+publisher VARCHAR(100),
+intellectual_rights VARCHAR(100),
+license VARCHAR(100),
+owner VARCHAR(100),
+original_source VARCHAR(100),
+PRIMARY KEY (id_droits),
+FOREIGN KEY (id_droits) REFERENCES observations(id_droits)
+);'
+dbSendQuery(con, creer_droits)
+
+# Injection des enregistrements dans la BD
+dbWriteTable(con, append = TRUE, name = "observations", value = table_obs, row.names = FALSE)
+dbWriteTable(con, append = TRUE, name = "temps", value = table_temps, row.names = FALSE)
+dbWriteTable(con, append = TRUE, name = "droits", value = table_droits, row.names = FALSE)

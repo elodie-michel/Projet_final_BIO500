ea36539b48c4462ff332c77695e09f3abfc0dfac
elodie-michel <elodie.michel@usherbrooke.ca>
2025-04-17 21:27:57 -0400

    Addition de l'objet quebec (limites géographiques du québec en shapefile) + addition de l'argument quebec à la fonction test_erreurs + addtion de "R/" pour montrer où est la base de donnees lepidopteres.db

--- lepidopteres/R/Script_principal.R
+++ lepidopteres/R/Script_principal.R
@@ -8,6 +8,9 @@ source("R/Script_figure_1.R")
 source("R/Script_figure_2.R")
 source("R/Script_figure_3.R")
 
+# Charger la carte du Québec pour le filtrage des coordonnées
+quebec <- st_read("donnees/donnees_cartographiques/bordure_quebec.shp")
+
 # Traitement de données
 dossier <- "./donnees"
 g_b_brut <- traiter_donnees(dossier)
@@ -17,7 +20,7 @@ g_b_brut <- traiter_donnees(dossier)
 g_b_nettoye<-nettoyer_les_donnees(g_b_brut)
 
 #Tests pour retirer les erreurs potentielles de la base de données principale
-g_b_final <- test_erreurs(g_b_nettoye)
+g_b_final <- test_erreurs(g_b_nettoye, quebec)
 
 #Étapes pour la création des tableaux 
 table_g_b <- creer_tables(g_b_final)
@@ -29,10 +32,10 @@ table_droits <- table_g_b$table_droits
 creer_base_de_donnees_SQL("lepidopteres.db", table_obs, table_temps, table_droits)
 
 #Créer la figure 1 de richesse spécifiques par année
-figure_richesse_temporelle("lepidopteres.db")
+figure_richesse_temporelle("R/lepidopteres.db")
 
 #Créer la figure 2 de richesse spécifique spatiale 
-figure_richesse_spatiale("lepidopteres.db", quebec)
+figure_richesse_spatiale("R/lepidopteres.db", quebec)
 
 #créer la figure 3 de phénologie 
-figure_phenologie("lepidopteres.db", 10)
+figure_phenologie("R/lepidopteres.db", 10)

2be013cf8975baa80a6a169166e8d2297bb9fda5
elodie-michel <elodie.michel@usherbrooke.ca>
2025-04-22 13:19:07 -0400

    changement de nom des bases de données

--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 library(targets)
 library(tarchetypes)
 
-tar_source("lepidopteres/R")  # équivalent à tous tes `source(...)` actuels
+tar_source("R")  # équivalent à tous tes source(...) actuels
 
 tar_option_set(
   packages = c("dplyr", "RSQLite", "lubridate", "data.table", "DBI", "sf", "ggplot2", "rmarkdown","knitr")
@@ -10,40 +10,40 @@ tar_option_set(
 list(
   tar_target(
     name = quebec,
-    command = st_read("lepidopteres/donnees/donnees_cartographiques/bordure_quebec.shp")
+    command = st_read("donnees/donnees_cartographiques/bordure_quebec.shp")
   ),
   tar_target(
     name = dossier,
-    command = "lepidopteres/donnees",
+    command = "donnees",
     format = "file"
   ),
   tar_target(
-    name = g_b_brut,
+    name = bd_brut,
     command = traiter_donnees(dossier)
   ),
   tar_target(
-    name = g_b_nettoye,
-    command = nettoyer_les_donnees(g_b_brut)
+    name = bd_nettoye,
+    command = nettoyer_les_donnees(bd_brut)
   ),
   tar_target(
-    name = g_b_final,
-    command = test_erreurs(g_b_nettoye,quebec)
+    name = bd_final,
+    command = test_erreurs(bd_nettoye,quebec)
   ),
   tar_target(
-    name = table_g_b,
-    command = creer_tables(g_b_final)
+    name = table_bd,
+    command = creer_tables(bd_final)
   ),
   tar_target(
     name = table_obs,
-    command = table_g_b$table_obs
+    command = table_bd$table_obs
   ),
   tar_target(
     name = table_temps,
-    command = table_g_b$table_temps
+    command = table_bd$table_temps
   ),
   tar_target(
     name = table_droits,
-    command = table_g_b$table_droits
+    command = table_bd$table_droits
   ),
   tar_target(
     name = base_sqlite,
@@ -68,9 +68,6 @@ list(
   ),
   tar_render(
     name = rapport,
-    path = "lepidopteres/rapport/rapport_final_BIO500/rapport_final_BIO500.Rmd",  # Mon fichier RMarkdown
+    path = "rapport_final_BIO500/rapport_final_BIO500.Rmd",  # Mon fichier RMarkdown
   )
 )
-
-
-

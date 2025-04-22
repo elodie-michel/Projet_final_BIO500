0786fedc267f97643be9b82e1b273e8175dd563e
elodie-michel <elodie.michel@usherbrooke.ca>
2025-04-22 13:19:55 -0400

    ajout de commentaires à la fonction du script nettoyage

--- lepidopteres/R/Script_nettoyage.R
+++ lepidopteres/R/Script_nettoyage.R
@@ -1,39 +1,37 @@
-#Nettoyage de données
-
-#pour nettoyer les données 
-nettoyer_les_donnees <- function(g_b, colonnes_a_supprimer) {
+# Fonction pour nettoyer les données d'observations de lépidoptères où bd est le nom donnée à la base de données
+nettoyer_les_donnees <- function(bd, colonnes_a_supprimer) {
   
-  # Ajouter la colonne is_valid, si une erreur est détecté dans une ligne, la valeur de is_valid deviendra FALSE pour pouvoir l'exclure des analyses.
-  g_b$is_valid <- TRUE #les donnees sont tous valides (inclues dans les analyses) jusqu'à preuve du contraire
-  g_b$is_valid <- as.logical(g_b$is_valid) #format logique pour la colonne (TRUE ou FALSE)
+  # Initialiser une colonne booléenne 'is_valid' pour indiquer si une ligne est valide (TRUE par défaut)
+  bd$is_valid <- TRUE 
+  bd$is_valid <- as.logical(bd$is_valid)  # S'assurer que la colonne est bien de type logique
   
-  # Avoir juste la date dans la colonne dwc_event_date
-  g_b$dwc_event_date <- substr(g_b$dwc_event_date, 1, 10)
+  # Extraire uniquement la date (AAAA-MM-JJ) de la colonne 'dwc_event_date'
+  bd$dwc_event_date <- substr(bd$dwc_event_date, 1, 10)
   
-  # Mettre les bons types de colonnes
-  g_b$observed_scientific_name <- as.character(g_b$observed_scientific_name)
-  g_b$dwc_event_date <- as.Date(g_b$dwc_event_date)
+  # Conversion des types de colonnes pour assurer une bonne manipulation :
+  bd$observed_scientific_name <- as.character(bd$observed_scientific_name)  # noms scientifiques en texte
+  bd$dwc_event_date <- as.Date(bd$dwc_event_date)  # dates au format Date
   
-  #Filtrer les donnees pour utiliser juste celles avec un nom scientifique d'espèce bien écrit
-  taxo <- read.csv("lepidopteres/donnees/taxonomie.csv")
-  noms_valides <- unique(taxo$observed_scientific_name[taxo$rank == "species"])
-  g_b$is_valid[!(g_b$observed_scientific_name %in% noms_valides)] <- FALSE
+  # Validation des noms scientifiques : marquer comme non valides les noms qui ne sont pas reconnus
+  taxo <- read.csv("donnees/taxonomie.csv")  # lecture du fichier de référence taxonomique
+  noms_valides <- unique(taxo$observed_scientific_name[taxo$rank == "species"])  # extraction des noms d'espèces valides
+  bd$is_valid[!(bd$observed_scientific_name %in% noms_valides)] <- FALSE  # marquer les noms non valides comme FALSE
   
-  #Séparer les presences et abundance pour uniformiser le tout
+  # Uniformisation des types de mesures ('presence' ou 'abundance') dans la colonne 'obs_variable'
+  bd$obs_variable <- tolower(bd$obs_variable)  # mettre tout en minuscule pour éviter les doublons liés à la casse
+  bd$obs_variable[bd$obs_variable %in% c("occurrence", "pr@#sence")] <- "presence"  # harmoniser les libellés erronés
+  bd$obs_variable[!bd$obs_variable %in% c("presence", "abundance")] <- NA  # remplacer les valeurs non reconnues par NA
   
-  g_b$obs_variable <- tolower(g_b$obs_variable)  # En minuscule, au cas où
-  g_b$obs_variable[g_b$obs_variable %in% c("occurrence", "pr@#sence")] <- "presence"
-  g_b$obs_variable[!g_b$obs_variable %in% c("presence", "abundance")] <- NA  # Mettre NA aux autres
+  # Conversion des coordonnées géographiques en format numérique
+  bd$lat <- as.numeric(bd$lat)
+  bd$lon <- as.numeric(bd$lon)
   
-  # Convertir les colonnes lat et lon en numériques
-  g_b$lat <- as.numeric(g_b$lat)
-  g_b$lon <- as.numeric(g_b$lon)
-  
-  # Fusionner les colonnes 'titre' et 'title' (on garde juste la colonne titre)
-  if ("title" %in% names(g_b)) {
-    g_b$titre <- ifelse(is.na(g_b$titre) | g_b$titre == "", g_b$title, g_b$titre)
-    g_b$title <- NULL
+  # Fusion des colonnes 'titre' et 'title' (on garde 'titre' comme colonne principale)
+  if ("title" %in% names(bd)) {
+    bd$titre <- ifelse(is.na(bd$titre) | bd$titre == "", bd$title, bd$titre)  # remplir les vides de 'titre' avec 'title'
+    bd$title <- NULL  # supprimer la colonne 'title'
   }
   
-  return(g_b)
+  # Retourner la base de données nettoyé
+  return(bd)
 }
